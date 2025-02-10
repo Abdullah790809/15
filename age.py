6f72dd28c50976aa84ea16d29a0c7341e7cec836
Abdullah790809 <790809@pdsb.net>
2025-02-10 12:38:06 -0500

    Create age.py

---
+++ age.py
@@ -0,0 +1,10 @@
+age = int(input("What is your age:"))
+
+  
+age2 = age * 2
+age2 = age2 + 5
+age2 = age2 * 50
+age3 = age2 + age
+age3 = age3 - 250
+age3 = age3 / 100
+print("Your age is: ", age3)
