a9eb48e5ecb11a07653972cfe369c548957333df
Abdullah790809 <790809@pdsb.net>
2025-02-10 13:01:14 -0500

    Create agee.py

---
+++ agee.py
@@ -0,0 +1,11 @@
+age = int(input("What is your age:"))
+year = int(input("What is your birth year: "))
+
+  
+year2 = year * 2
+year2 = year2 + 5
+year2 = year2 * 50
+yage = year2 + age
+yage = yage - 250
+yage = yage / 100
+print("Your Number is: " , yage)
