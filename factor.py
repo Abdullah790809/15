221a0f37517918a451433f989c4c7c418f45f66a
Abdullah790809 <790809@pdsb.net>
2025-02-12 12:44:28 -0500

    Update factor.py

--- factor.py
+++ factor.py
@@ -13,3 +13,29 @@ if rem == 0:
     print("Yes!", y, "is a factor of", x)
 else:
   print("No", y, "is not of factor of", x)
+
+
+
+
+
+
+
+import math
+
+x = input("Please input a whole number between 1 and 20: ")
+x = int(x)
+y = input("Please input another nonzero whole number between 1 and 20")
+y = int(y)
+if (y <= 0) and (y >= 20):
+  y = int(input("Please input a nonzero whole between 1 and 20: "))
+else: print( y, "is out of range")
+if (x <= 0) and (x >= 20):
+  x = int(input("Please input a nonzero whole between 1 and 20: "))
+  print("Now deciding if", y, "is a factor of", x, "...")
+else:
+  print( x, "is out of range")
+rem = x % y
+if rem == 0:
+    print("Yes!", y, "is a factor of", x)
+else:
+  print("No", y, "is not of factor of", x)
