24fdaea5bd8ad8b1039dc9045610bcd270b500be
Abdullah790809 <790809@pdsb.net>
2025-03-01 15:06:14 -0500

    Update area.py

--- area.py
+++ area.py
@@ -1,11 +1,10 @@
 import math
 length = input("Please input a length: ")
-radius = (leanth / 2)
-radius = float(radius)
+radius = int(input("Please input a radius: "))
 length = float(length)
 area = math.pow(length, 2)
-area2 = (3.14 * radius)
+area2 = 3.14 * radius
 area3 = math.pow(area2, 2)
-area4 = (0.5 * area3)
-area5 = (area4 + area)
+area4 = 0.5 * area3
+area5 = area4 + area
 print("The area of the object is: ", area5)
