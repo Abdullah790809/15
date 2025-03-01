dbbe1b13fb27f56cdd7a35261794dafbc5a53d71
Abdullah790809 <790809@pdsb.net>
2025-03-01 16:49:48 -0500

    Update outputmath.py

--- outputmath.py
+++ outputmath.py
@@ -1,5 +1,8 @@
-num = input("Please input a whole number: ") 
-num3 = 15
-num = int(num) 
+num = int(input("Please input a whole number: "))
+
+num3 = int(input("Please input a non zero whole number: "))
+
 num2 = num / num3
-print("When", num, " is divided by 15", "the result is:", num2)
+
+
+print("When", num , " is divided by ", num3 , "the result is:", num2)
