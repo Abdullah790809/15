627f8c615e20ea6d7a70746aa9bd84c75baedd10
Abdullah790809 <790809@pdsb.net>
2025-06-16 13:04:05 -0400

    Update assiangment4.py

--- assiangment4.py
+++ assiangment4.py
@@ -1,22 +1,31 @@
-
-# data_lines   : List that holds each line of file data (split into parts)
+"""
+  Author : Abdullah Parvez
+  Student Number : 790809
+  Revison date : 2 March 2025
+  Program : Even or Odd
+  Description : A program that determines whether the product of number is even or odd
+VARIABLE DICTIONARY
+ # data_lines   : List that holds each line of file data (split into parts)
 # dates        : List that holds the converted numeric dates (format: yyyymmdd)
 # answers      : List that holds the corresponding Wordle words
 # months       : List of month abbreviations used for converting text to numbers
 # num          : Numeric representation of a date (used in to_number function)
 # word         : The word the user is searching for (used in search_word)
-# mon, dy, yr  : Month, day, and year input by the user (used in search_date)
+#= mon, dy, yr  : Month, day, and year input by the user (used in search_date)
 # target       : Numeric version of the user's input date (used in search_date)
 # file_loaded  : Boolean flag to check if file opened successfully
 # file         : File object used to read "wordle.dat"
-# line         : Current line being read from the file
-# entry        : One row from data_lines being processed (contains date + word)
-# option       : User’s menu selection – 'w' for word search, 'd' for date search
-# user_word    : Word entered by the user when searching by word
-# found        : Numeric date found for a word (from search_word)
-# input_date   : Numeric version of a user-entered date (for validation)
-# result       : Word found for a given date (from search_date)
-# ---------------------------------------------------------
+ line         : Current line being read from the file
+ entry        : One row from data_lines being processed (contains date + word)
+ option       : User’s menu selection – 'w' for word search, 'd' for date search
+ user_word    : Word entered by the user when searching by word
+ found        : Numeric date found for a word (from search_word)
+ input_date  : Numeric version of a user-entered date (for validation)
+ result       : Word found for a given date (from search_date)
+---------------------------------------------------------
+
+"""
+
 
 
 # List to hold lines read from the file
